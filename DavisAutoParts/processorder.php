614451a2f963a6617e33031f7b9324509163087c
davorin1 <davorin.prislin@gmail.com>
2017-11-24 15:41:27 +0100

    davis auto parts

---
+++ DavisAutoParts/processorder.php
@@ -0,0 +1,28 @@
+<?php
+    // create short variable names
+    $tireqty = $_POST['tireqty'];
+    $oilqty = $_POST['oilqty'];
+    $sparkqty = $_POST['sparkqty'];
+
+    $totalqty = 0;
+    $totalamount = 0.00;
+?>
+
+<html>
+<head>
+    <title>Bob’s Auto Parts - Order Results</title>
+</head>
+<body>
+<h1>Bob’s Auto Parts</h1>
+<h2>Order Results</h2>
+<?php
+    echo '<p>Order processed at ';
+    echo date('H:i, jS F Y');
+    echo '</p>';
+    echo '<p>Your order is as follows: </p>';
+    echo $tireqty.' tires<br/>';
+    echo $oilqty.' bottles of oil<br/>';
+    echo $sparkqty.' spark plugs<br/>';
+?>
+</body>
+</html>
